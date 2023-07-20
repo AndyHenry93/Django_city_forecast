c1a51744ab88133fe40aa2ad178e2c326ae2526d
Andyhenry93 <A.henry250@gmail.com>
2023-07-20 15:56:40 -0400

    made adjustments to google credentials

--- forecast/utils.py
+++ forecast/utils.py
@@ -21,17 +21,17 @@ def get_data(range):
     return: the dataset 
     """
     credentials = {
-            "type": str(os.getenv('service_account')),
-            "project_id": str(os.getenv('project_id')),
-            "private_key_id": str(os.getenv('private_key_id')),
-            "private_key": str(os.getenv('private_key')),
-            "client_email": str(os.getenv('client_email')),
-            "client_id": str(os.getenv('client_id')),
-            "auth_uri": str(os.getenv('auth_uri')),
-            "token_uri": str(os.getenv('token_uri')),
-            "auth_provider_x509_cert_url": str(os.getenv('auth_provider_x509_cert_url')),
-            "client_x509_cert_url": str(os.getenv('client_x509_cert_url')),
-            "universe_domain": str(os.getenv('universe_domain')),
+            "type": (os.getenv('service_account')),
+            "project_id": (os.getenv('project_id')),
+            "private_key_id": (os.getenv('private_key_id')),
+            "private_key": (os.getenv('private_key')),
+            "client_email": (os.getenv('client_email')),
+            "client_id": (os.getenv('client_id')),
+            "auth_uri": (os.getenv('auth_uri')),
+            "token_uri": (os.getenv('token_uri')),
+            "auth_provider_x509_cert_url": (os.getenv('auth_provider_x509_cert_url')),
+            "client_x509_cert_url": (os.getenv('client_x509_cert_url')),
+            "universe_domain": (os.getenv('universe_domain')),
     }
     # Using gspread to call the google sheets api and saving the dataset to the data variable
     gc = gspread.service_account_from_dict(credentials)
